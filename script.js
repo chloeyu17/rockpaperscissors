8f0bceaf25df7563221af420c89d311c22bb7163
cyu <chloeyu100@gmail.com>
2021-02-20 14:18:36 -0500

    can type in rock, Rock, r, or R as user input

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 var userChoice;
-
 var computerChoice;
-
 var message;
+var rock = ["R", "Rock", "r", "rock"];
+var paper = ["P", "Paper", "p", "paper"];
+var scissors = ["S", "Scissors", "s", "scissors"];
 
+//Allows the user to set userChoice
 function user(){
-    userChoice = prompt("Do you choose Rock, Paper or Scissors?");
-    return userChoice;
+    return userChoice = prompt("Do you choose Rock, Paper or Scissors?");
 }
 
-
+//Sets computerChoice randomly
 function computer(){
     computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "R";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "P";
-} else {
-	computerChoice = "S";
-}
+    if (computerChoice < 0.34) {
+        computerChoice = "R";
+    } else if(computerChoice <= 0.67) {
+        computerChoice = "P";
+    } else {
+        computerChoice = "S";
+    }
 }
 
+//Alert
 function play(){
     return window.alert("Let's play Rock Paper Scissors!");
 }
 
+//Win message
 function win(){
-    return message = "You win, play again";
+    return message = "You win!";
 }
 
+//Lose message
 function lose(){
-    return message = "You lose, play again";
+    return message = "You lose!";
 }
 
+//Tie message
 function tie(){
-    return message = "You tied, play again";
+    return message = "You tied!";
 }
 
+//Compares user input with computer-generated choice
 function result(){
     window.alert(compare(userChoice,computerChoice));
     return compare(userChoice,computerChoice);
@@ -48,8 +54,8 @@ function compare(choice1,choice2){
         return tie();
     }
     //User chose rock, computer chose scissors
-    else if(choice1==="R"){
-        if(choice2==="S"){
+    else if(rock.includes(choice1)){
+        if(scissors.includes(choice2)){
             return win();
         }
         //user chose rock, computer chose paper
@@ -58,24 +64,23 @@ function compare(choice1,choice2){
         }
     }
     //user chose paper, computer chose rock
-    else if(choice1==="P"){
-        if(choice2==="R"){
+    else if(paper.includes(choice1)){
+        if(rock.includes(choice2)){
             return win();
         }
         //user chose paper, computer chose scissors
         else {
-            return lose;
+            return lose();
         }
     }
     //user chose scissors computer chose paper
-    else if(choice1==="S"){
-        if(choice2==="P"){
-            win();
+    else if(scissors.includes(choice1)){
+        if(paper.includes(choice2)){
+           return win();
         }
-
         //user chose scissors computer chose rock
         else {
-            lose();
+           return lose();
         }
     } else {
         return "Input R, P, or S!";
